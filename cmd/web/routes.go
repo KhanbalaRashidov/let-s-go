6f24768533467f19f18b89939ed62360ea8cbf03
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-07-27 08:11:05 +0400

    Added middleware.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
@@ -11,5 +14,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
